008268938adcfbfe030d67976c03d7a24efef1ca
Loic Chagnoleau <loic.chagnoleau@gmail.com>
2020-12-04 10:55:35 +0100

    FreeRTOS Tasks works

--- app/src/main.c
+++ app/src/main.c
@@ -2,16 +2,78 @@
 #include "stm32f4xx.h"
 #include "board.h"
 #include "uart.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* MACROS */
+#define AVAILABLE 1
+#define NON_AVAILABLE 0
+
+
+TaskHandle_t xTaskHande1 = NULL;
+TaskHandle_t xTaskHande2 = NULL;
+
+uint8_t UART_ACCESS_KEY = AVAILABLE;
+
+void vTask1_handler(void *params);
+void vTask2_handler(void *params);
 
 int main(void)
 {
     /* Hardware board init */
     board_hardware_init();
 
+    send_data("Hello world ! \r\n");
+
     while(1)
     {
-        send_data("Hello world ! \r\n");
+        /* Create 2 tasks */
+        xTaskCreate(
+            vTask1_handler,
+            "Task-1",
+            configMINIMAL_STACK_SIZE,
+            NULL,
+            2,
+            &xTaskHande1);
+        xTaskCreate(
+            vTask2_handler,
+            "Task-2",
+            configMINIMAL_STACK_SIZE,
+            NULL,
+            2,
+            &xTaskHande2);
+
+        /* Start scheduler */
+        vTaskStartScheduler();
     }
 
     return 0;
+}
+
+void vTask1_handler(void *params)
+{
+	while(1)
+	{
+		if(UART_ACCESS_KEY == AVAILABLE)
+		{
+			UART_ACCESS_KEY = NON_AVAILABLE;
+			send_data("Hello world from Task 1\r\n");
+			UART_ACCESS_KEY = AVAILABLE;
+			taskYIELD();
+		}
+	}
+}
+
+void vTask2_handler(void *params)
+{
+	while(1)
+	{
+		if(UART_ACCESS_KEY == AVAILABLE)
+		{
+			UART_ACCESS_KEY = NON_AVAILABLE;
+			send_data("Hello world from Task 2\r\n");
+			UART_ACCESS_KEY = AVAILABLE;
+			taskYIELD();
+		}
+	}
 }
